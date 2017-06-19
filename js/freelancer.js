ca9a05642bd31b48ca7b9a7908c85e966456aae2
Wendy Conditions <wam922@gmail.com>
2017-06-18 21:04:00 -0700

    Update freelancer.js

--- js/freelancer.js
+++ js/freelancer.js
@@ -1,4 +1,21 @@
 // Freelancer Theme JavaScript
+        var countDownDate = new Date("Aug 24, 2017 19:00:00").getTime();
+
+        var x = setInterval(function () {
+            var now = new Date().getTime();
+            var distance = countDownDate - now;
+            var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+            document.getElementById("countdown").innerHTML = days + " days " + hours + " hours " + minutes + " minutes " + seconds + " seconds ";
+
+            if (distance < 0) {
+                clearInterval(x);
+                document.getElementById("countdown").innerHTML = "Let the show begin!";
+            }
+        }, 1000);
 
 (function($) {
     "use strict"; // Start of use strict
